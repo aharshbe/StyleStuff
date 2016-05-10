1eba48214c02efe6579cb56f4a33826d7ec906d8
aharshbe <a.catawampus@gmail.com>
2016-05-10 11:19:00 -0700

    Adding api level for reference later

---
+++ apilevel/app/src/main/java/com/example/austin/myapplication/Languages.java
@@ -0,0 +1,20 @@
+package com.example.austin.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Languages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_languages);
+    }
+
+    public void clickingFrench(View view) {
+    }
+
+    public void clickingHindi(View view) {
+    }
+}
